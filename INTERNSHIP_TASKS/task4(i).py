53c86a6b4bf42277ff3962061971448a4d5be4f0
Dev Bheda <bhedadev010@gmail.com>
2024-07-26 09:41:47 +0530

    4-7-24

---
+++ INTERNSHIP_TASKS/task4(i).py
@@ -0,0 +1,266 @@
+#ALL THE MESSAGE INFO IS CONVERTED INTO STRING
+
+
+mydata={
+  "status": "SUCCESS",
+  "message": "null",
+  "data": {
+    "count": 10,
+    "articles": [
+      {
+        "hashId": "vzbgx6av-1",
+        "oldHashId": "10-bridge-collapse-incidents-reported-in-bihar-in-15-days-1720091645071",
+        "authorName": "Shreyasi Banerjee",
+        "content": "As many as 10 bridge collapse incidents have been reported in Bihar in the last 15 days. A petition has been filed in the Supreme Court seeking directions to the Bihar government to conduct a structural audit of all existing and under construction bridges in the state. \"A high-level probe has been ordered,\" Saran District Magistrate Aman Samir said.",
+        "sourceName": "Moneycontrol",
+        "sourceUrl": "https://www.moneycontrol.com/news/india/bihar-bridges-are-falling-down-10th-collapse-in-15-days-shocks-state-12762150.html/amp?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts",
+        "title": "10 bridges collapse in 15 days in Bihar",
+        "subtitle": "To know in which districts bridges were located",
+        "imageUrl": "https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2024/07_jul/4_thu/img_1720090420821_38.jpg?",
+        "shortenedUrl": "https://shrts.in/Ngpmo",
+        "categoryNames": [
+          "national"
+        ],
+        "relevancyTags": [
+          "national"
+        ],
+        "language": "ENGLISH",
+        "countryCode": "IN",
+        "score": 1000,
+        "important": "false",
+        "createdAt": 1720091645000
+      },
+      {
+        "hashId": "ufsvzc5p-1",
+        "oldHashId": "pm-modi-meets-t20-world-cup-2024-winning-team-india--poses-with-trophy-1720079950243",
+        "authorName": "Anmol Sharma",
+        "content": "Prime Minister Narendra Modi on Thursday met T20 World Cup 2024-winning Team India at his residence in Delhi. He posed with the trophy alongside the squad. He also interacted with the squad. Team India arrived in Delhi today morning after a 15-hour journey from Barbados. The team will take part in a victory parade in the evening in Mumbai.",
+        "sourceName": "ANI",
+        "sourceUrl": "https://x.com/ANI/status/1808767618838638739?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts",
+        "title": "PM Modi meets T20 World Cup 2024-winning Team India, poses with trophy",
+        "subtitle": "India defeated South Africa in the final",
+        "imageUrl": "https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2024/07_jul/4_thu/img_1720079515560_817.jpg?",
+        "shortenedUrl": "https://shrts.in/5dEHm",
+        "categoryNames": [
+          "T20_WORLD_CUP_2024",
+          "sports"
+        ],
+        "relevancyTags": [],
+        "language": "ENGLISH",
+        "countryCode": "IN",
+        "score": 1000,
+        "important": "false",
+        "createdAt": 1720079950000
+      },
+      {
+        "hashId": "bcadtlrk-1",
+        "oldHashId": "ls-speaker-birla-amends-oath-taking-rules-after--jai-palestine----jai-hindu-rashtra--slogans-1720074200339",
+        "authorName": "Shreyasi Banerjee",
+        "content": "Lok Sabha Speaker Om Birla has constituted a committee to frame rules for \"oath-taking and affirmation\" for members. Members won't be allowed to use any word or expression as a prefix or suffix to the form of oath. Earlier, AIMIM President Asaduddin Owaisi, ended his oath with chants of \"Jai Palestine\", while BJP's Chhatrapal Singh Gangwar said \"Jai Hindu Rashtra\".",
+        "sourceName": "Times Now",
+        "sourceUrl": "https://www.timesnownews.com/india/now-members-cant-raise-slogans-while-taking-oath-check-lok-sabha-speakers-new-amendment-of-rule-article-111467634/amp?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts",
+        "title": "LS Speaker Birla amends oath-taking rules after 'Jai Palestine', 'Jai Hindu Rashtra' slogans",
+        "subtitle": "'Jai Samvidhan' slogan was also raised",
+        "imageUrl": "https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2024/07_jul/4_thu/img_1720072328481_203.jpg?",
+        "shortenedUrl": "https://shrts.in/FHvUp",
+        "categoryNames": [
+          "politics",
+          "national"
+        ],
+        "relevancyTags": [
+          "national",
+          "politics"
+        ],
+        "language": "ENGLISH",
+        "countryCode": "IN",
+        "score": 1000,
+        "important": "false",
+        "createdAt": 1720074200000
+      },
+      {
+        "hashId": "lgvfjkef-1",
+        "oldHashId": "33-000-indians-killed-per-year-due-to-air-pollution-in-10-cities--delhi-tops-list--study-1720068260206",
+        "authorName": "Sakshita Khosla",
+        "content": "Over 33,000 deaths/year in 10 Indian cities can be attributed to air pollution, a new study in Lancet Planetary Health said. Of these, Delhi has the highest air pollution-linked annual mortality burden at around 11.5% or around 12,000 deaths per year. The other cities analysed in the study are Ahmedabad, Bengaluru, Chennai, Hyderabad, Kolkata, Mumbai, Pune, Shimla and Varanasi.",
+        "sourceName": "Indian Express",
+        "sourceUrl": "https://indianexpress.com/article/india/air-pollution-behind-7-of-deaths-in-10-cities-lancet-study-9431526/lite/?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts",
+        "title": "33,000 Indians killed per year due to air pollution in 10 cities, Delhi tops list: Study",
+        "subtitle": "'PM2.5 levels above safe limit on 99.8% days'",
+        "imageUrl": "https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2024/07_jul/4_thu/img_1720066684152_647.jpg?",
+        "shortenedUrl": "https://shrts.in/xhVah",
+        "categoryNames": [
+          "science",
+          "national"
+        ],
+        "relevancyTags": [
+          "national",
+          "science"
+        ],
+        "language": "ENGLISH",
+        "countryCode": "IN",
+        "score": 1000,
+        "important": "false",
+        "createdAt": 1720068260000
+      },
+      {
+        "hashId": "jgaihg9c-1",
+        "oldHashId": "sensex--nifty-open-at-new-record-highs-1720066423391",
+        "authorName": "Anmol Sharma",
+        "content": "The benchmark equity indices opened at new highs on Thursday. The BSE Sensex climbed 336.51 points, or 0.42%, to 80,323.30 in the opening trade, while the NSE Nifty 50 opened up 83.50 points, or 0.34%, at 24,370.00. Meanwhile, the Nifty Bank index opened at 53,357.70, up 268.45 points, or 0.51%, from its previous closing position.\n",
+        "sourceName": "CNBC-TV18",
+        "sourceUrl": "https://x.com/CNBCTV18Live/status/1808708447946346761?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts",
+        "title": "Sensex, Nifty open at new record highs",
+        "subtitle": "Nifty Midcap 100 opened at 56,510.30",
+        "imageUrl": "https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2024/07_jul/4_thu/img_1720065574531_914.jpg?",
+        "shortenedUrl": "https://shrts.in/anpwY",
+        "categoryNames": [
+          "business"
+        ],
+        "relevancyTags": [
+          "business"
+        ],
+        "language": "ENGLISH",
+        "countryCode": "IN",
+        "score": 1000,
+        "important": "false",
+        "createdAt": 1720066423000
+      },
+      {
+        "hashId": "mbw35gos-1",
+        "oldHashId": "how-will-voting-take-place-in-uk-general-elections-today--1720063823360",
+        "authorName": "Sakshita Khosla",
+        "content": "UK, that operates a \"first past the post\" electoral system, will vote in national elections today. UK has 650 seats in House of Commons, each represented by an MP. Voters elect representatives on the 650 seats and leader of the party with the most seats becomes Prime Minister. A party needs to win at least 326 seats to win majority. ",
+        "sourceName": "Financial Express",
+        "sourceUrl": "https://www.financialexpress.com/world-news/uk-election-2024-explained-how-they-work-key-issues-and-implications/3542679/lite/?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts",
+        "title": "How will voting take place in UK general elections today?",
+        "subtitle": "Rishi Sunak-led Conservative Party is in power",
+        "imageUrl": "https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2024/07_jul/4_thu/img_1720063002724_624.jpg?",
+        "shortenedUrl": "https://shrts.in/ipmoc",
+        "categoryNames": [
+          "politics",
+          "EXPLAINERS",
+          "world"
+        ],
+        "relevancyTags": [
+          "world",
+          "politics"
+        ],
+        "language": "ENGLISH",
+        "countryCode": "IN",
+        "score": 1000,
+        "important": "false",
+        "createdAt": 1720063823000
+      },
+      {
+        "hashId": "z3dv8xcf-1",
+        "oldHashId": "rohit-sharma-flaunts-t20-world-cup-trophy-after-landing-at-delhi-airport-1720058003433",
+        "authorName": "Anmol Sharma",
+        "content": "Team India captain Rohit Sharma flaunted the T20 World Cup 2024 trophy after landing at Delhi airport. Hundreds of fans gathered at the airport to welcome the T20 World Cup 2024-winning squad. Team India squad members, including Virat Kohli, Suryakumar Yadav and Rishabh Pant, were seen wearing their World Cup medals as they walked out of the airport.",
+        "sourceName": "PTI",
+        "sourceUrl": "https://x.com/PTI_News/status/1808676087280353280?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts",
+        "title": "Rohit Sharma flaunts T20 World Cup trophy after landing at Delhi airport",
+        "subtitle": "Team India squad to meet PM Modi today",
+        "imageUrl": "https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2024/07_jul/4_thu/img_1720057756365_496.jpg?",
+        "shortenedUrl": "https://shrts.in/nRCiK",
+        "categoryNames": [
+          "T20_WORLD_CUP_2024",
+          "sports"
+        ],
+        "relevancyTags": [
+          "cricket"
+        ],
+        "language": "ENGLISH",
+        "countryCode": "IN",
+        "score": 1000,
+        "important": "true",
+        "createdAt": 1720058003000
+      },
+      {
+        "hashId": "1d9hp6gd-1",
+        "oldHashId": "plane-carrying-world-cup-winning-team-india-lands-in-delhi-after-15-hour-journey-1720054483867",
+        "authorName": "Anmol Sharma",
+        "content": "The Air India special chartered flight 'AIC24WC' carrying T20 World Cup 2024-winning Team India squad and support staff landed in Delhi after a 15-hour journey. The team's departure from Barbados was delayed due to Hurricane Beryl. The special flight was arranged by BCCI. India clinched their second T20 World Cup title by defeating South Africa in final on June 29.",
+        "sourceName": "ANI",
+        "sourceUrl": "https://x.com/ANI/status/1808661929516544216?utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts",
+        "title": "Plane carrying World Cup-winning Team India lands in Delhi after 15-hour journey",
+        "subtitle": "PM Modi will felicitate the team today",
+        "imageUrl": "https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2024/07_jul/4_thu/img_1720054378190_9.jpg?",
+        "shortenedUrl": "https://shrts.in/dBkVK",
+        "categoryNames": [
+          "T20_WORLD_CUP_2024",
+          "sports"
+        ],
+        "relevancyTags": [
+          "cricket"
+        ],
+        "language": "ENGLISH",
+        "countryCode": "IN",
+        "score": 1000,
+        "important": "false",
+        "createdAt": 1720054483000
+      },
+      {
+        "hashId": "jd96pecm-1",
+        "oldHashId": "bjp-veteran-lk-advani-hospitalised-again--days-after-getting-discharged-from-aiims-1720026821235",
+        "authorName": "Deepika Bhatt",
+        "content": "Veteran BJP leader Lal Krishna Advani was admitted to Apollo Hospital in Delhi on Wednesday. The 96-year-old leader was earlier admitted to the All India Institute of Medical Sciences (AIIMS) in Delhi for treatment in the Department of Urology and was discharged on June 27. \"He is under observation and stable,\" said Apollo Hospital.",
+        "sourceName": "ANI",
+        "sourceUrl": "https://x.com/ANI/status/1808544461120786595?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Etweet&utm_campaign=fullarticle&utm_medium=referral&utm_source=inshorts",
+        "title": "BJP veteran LK Advani hospitalised again, days after getting discharged from AIIMS",
+        "subtitle": "More details are awaited",
+        "imageUrl": "https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2024/07_jul/3_wed/img_1720026337361_804.jpg?",
+        "shortenedUrl": "https://shrts.in/JQzq5",
+        "categoryNames": [
+          "politics",
+          "national"
+        ],
+        "relevancyTags": [
+          "politics",
+          "national"
+        ],
+        "language": "ENGLISH",
+        "countryCode": "IN",
+        "score": 1000,
+        "important": "false",
+        "createdAt": 1720026821000
+      },
+      {
+        "hashId": "fusjetee-1",
+        "oldHashId": "scaler-s-pgp-mt-is-reshaping-business-education-in-india--scaler-1719897077490",
+        "authorName": "Aarish",
+        "content": "Based in Bangalore, Scaler School of Business announced that its 18-month, full-time PGP in Management & Tech has 1200+ employment partners from top MNCs and startups. It revealed a whopping 96% placement rate and median CTC of 25L for its online programs. Applications for the founding cohort, starting in August 2024, are still open and offer up to 100% scholarships",
+        "sourceName": "Scaler",
+        "sourceUrl": "https://ad.doubleclick.net/ddm/trackclk/N1896469.4299435DBM_SCALER/B32217896.398979795;dc_trk_aid=590499251;dc_trk_cid=218209903;dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;ltd=;dc_tdv=1",
+        "title": "Scaler's PGP-MT is reshaping business education in India: Scaler",
+        "subtitle": "Learn about program details, scholarships & more",
+        "imageUrl": "https://static.inshorts.com/inshorts/images/v1/variants/jpg/m/2024/07_jul/2_tue/img_1719896938730_815.jpg?",
+        "shortenedUrl": "https://shrts.in/GsdPP",
+        "categoryNames": [
+          "education"
+        ],
+        "relevancyTags": [],
+        "language": "ENGLISH",
+        "countryCode": "IN",
+        "score": 1000,
+        "important": "false",
+        "createdAt": 1719897077000
+      }
+    ]
+  }
+}
+
+
+
+print(mydata.keys())
+
+print(mydata.values())
+
+print(mydata.items())
+
+print(len(mydata.keys()))
+
+print(len(mydata.values()))
+
+print(mydata["data"])
+print(mydata["message"])
+print(mydata["status"])
